5e8f51ea7d36a1135c74612b1580e6bee68507b4
yairhdz <yairhdz0416@gmail.com>
2015-12-05 15:32:47 -0600

    Diseño del view para desencriptar

--- Criptography/DesencriptarViewController.swift
+++ Criptography/DesencriptarViewController.swift
@@ -8,15 +8,50 @@
 
 import UIKit
 
-class DesencriptarViewController: UIViewController {
+class DesencriptarViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textToDecrypt: UITextField!
+    @IBOutlet weak var keyToDecrypt: UITextField!
+    @IBOutlet weak var decryptButton: UIButton!
+    @IBOutlet weak var decryptedText: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.textToDecrypt.delegate = self
+        self.keyToDecrypt.delegate = self
         Utility.addGradientBackGround(self.view)
+        Utility.formatButtom(decryptButton, frame: CGRectMake(self.view.frame.width / 2 - 50, self.view.frame.height / 2 - 30, 100, 100), cornerRadius: 50.0)
+        
+        self.decryptedText.frame = CGRectMake(35, 400, 250, 100)
+        self.decryptedText.editable = false
+        self.decryptedText.layer.cornerRadius = 8.0
+        self.decryptedText.layer.borderColor = UIColor.whiteColor().CGColor
+        self.decryptedText.layer.backgroundColor = nil
+        self.decryptedText.text = nil
+        
+        self.textToDecrypt.returnKeyType  = .Done
+        self.keyToDecrypt.returnKeyType   = .Done
+        self.keyToDecrypt.secureTextEntry = true
+
     }
     
+    @IBAction func decryptAction(sender: UIButton) {
+        self.decryptedText.layer.borderWidth = 1.0
+        self.decryptedText.text = self.textToDecrypt.text
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        textToDecrypt.resignFirstResponder()
+        keyToDecrypt.resignFirstResponder()
+    }
 
     /*
     // MARK: - Navigation
