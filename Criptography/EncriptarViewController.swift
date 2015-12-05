c3b0108e4978b0fc6c8c629fa0392e4adc5c8dab
yairhdz <yairhdz0416@gmail.com>
2015-12-05 14:55:14 -0600

    Funcionalidad para ocultar teclado de textfields cuando se toca el fondo o al dar enter, funcionalidad bàsica del boton encriptar

--- Criptography/EncriptarViewController.swift
+++ Criptography/EncriptarViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EncriptarViewController: UIViewController {
+class EncriptarViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textToEncript: UITextField!
     @IBOutlet weak var keyToEncript: UITextField!
@@ -19,15 +19,34 @@ class EncriptarViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.textToEncript.delegate = self
+        self.keyToEncript.delegate = self
+        
         Utility.addGradientBackGround(self.view)
         Utility.formatButtom(encriptarButton, frame: CGRectMake(self.view.frame.width / 2 - 50, self.view.frame.height / 2 - 30, 100, 100), cornerRadius: 50.0)
+        
         self.encriptedText.frame = CGRectMake(35, 400, 250, 100)
         self.encriptedText.editable = false
-        self.encriptedText.layer.borderWidth = 1.0
         self.encriptedText.layer.cornerRadius = 8.0
         self.encriptedText.layer.borderColor = UIColor.whiteColor().CGColor
         self.encriptedText.layer.backgroundColor = nil
         self.encriptedText.text = nil
+    }
+    
+    @IBAction func encriptarAction(sender: UIButton) {
+        self.encriptedText.layer.borderWidth = 1.0
+        self.encriptedText.text = self.textToEncript.text
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        textToEncript.resignFirstResponder()
+        keyToEncript.resignFirstResponder()
     }
     
     /*
